809063efe22320c51c9538b49b299bac160e4e70
TomislavSmit <tomasmit@yahoo.com>
2025-02-06 23:24:40 +0100

    Fix testing setup

--- jest.config.ts
+++ jest.config.ts
@@ -14,7 +14,6 @@ const config: Config = {
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
     transform: {},
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
